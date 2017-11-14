aeacc8e21efc643117b3c6296fe869340bee5585
Michelle Chang <mchang98@uw.edu>
2017-11-14 14:03:51 -0800

    scatterplot function

--- exercise-1/scripts/BuildScatter.R
+++ exercise-1/scripts/BuildScatter.R
@@ -8,3 +8,12 @@
 #   - Title of the plot (set a default of "Title")
 #   - Label for the x axis (set a default of "X Title")
 #   - Label for the y axis (set a default of "Y Title")
+
+BuildScatterplot <- function(data, xaxis, yaxis, color, title, xtitle, ytitle) {
+  p <- plot_ly(data = data, x = ~xaxis, y = ~yaxis,
+               marker = list(color = color)) %>%
+    layout(title = title,
+           yaxis = xtitle,
+           xaxis = ytitle)
+  return(p)
+}
